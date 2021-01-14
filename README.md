10c11489f3ac0418f5e390c789cae8d76c81638a
krispykreme14 <72890029+krispykreme14@users.noreply.github.com>
2021-01-13 18:15:09 -0800

    Basic project framework in place: raw data display in table

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # covidinfo
+
+## Goals
+Phase 1: Display total cases and total deaths per state in a table
+Phase 2: Display data from Phase 1 on a map
---
+++ main.py
@@ -0,0 +1,29 @@
+
+from flask import Flask, render_template
+import requests
+#create a Flask instance
+app = Flask(__name__)
+
+#connects default URL of server to render home.html
+@app.route('/')
+def home_route():
+  data = get_current_state_data()
+  return render_template("home.html", data=data)
+
+
+def get_current_state_data():
+    response = requests.get("https://api.covidtracking.com/v1/states/current.json")
+    remotedata = response.json()
+    data = []
+    for element in remotedata:
+        data.append({ 'state': element['state'], 'positive': element['positive'], 'death': element['death']})
+
+    # data = [
+    #   { 'state': 'CA', 'positive': 2781039, 'death': 31102},
+    #   { 'state': 'AZ', 'positive': 641729, 'death': 10673}
+    # ]
+    return data
+
+if __name__ == "__main__":
+  #runs the application on the repl development server
+  app.run(debug=True, port='3000', host='127.0.0.1')
